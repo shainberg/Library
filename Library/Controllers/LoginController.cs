2da7f5877262c68c8efe27dce54acdd7106ef403
Or Shainberg <or.shainberg10@gmail.com>
2015-10-06 18:01:25 +0300

    Login

---
+++ Library/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            string id = this.Request["id"];
+            string password = this.Request.Params["password"];
+
+            if (id != null && password != null){
+                /*Query From users table*/
+                HttpContext.Session.Add("Connected", id);
+            }
+
+
+            if (HttpContext.Session["Connected"] != null && HttpContext.Session["Connected"] != string.Empty){
+                return View("../Home/Index", "_Layout");
+            }
+
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}

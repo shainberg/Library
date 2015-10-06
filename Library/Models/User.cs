6a94c6b1dab62c28c299b860c090789dc59b4b44
adisimantov <sadis66@gmail.com>
2015-10-06 15:45:48 +0300

    user model

---
+++ Library/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Library.Models
+{
+    public class User
+    {
+        [Key]
+        [DisplayName("ID")]
+        public int id { get; set; }
+
+        [DisplayName("password")]
+        public string password { get; set; }
+     
+    }
+}

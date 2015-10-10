78b047ae8bbc28da60dedc4d0965781f9e2b3c94
adisimantov <sadis66@gmail.com>
2015-10-10 23:17:52 +0300

    bundle

--- Library/App_Start/BundleConfig.cs
+++ Library/App_Start/BundleConfig.cs
@@ -8,8 +8,17 @@ namespace Library
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/paradise").Include(
+                        "~/Scripts/paradise*"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/bootstrap*",
+                         "~/Scripts/d3*",
+                        "~/Scripts/jquery.dataTables*",
+                        "~/Scripts/dataTables.bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -20,12 +29,16 @@ namespace Library
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/bootstrap*",
+                      "~/Scripts/d3*",
+                      "~/Scripts/jquery.dataTables*",
+                      "~/Scripts/dataTables.bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/books.css"));
         }
     }
 }

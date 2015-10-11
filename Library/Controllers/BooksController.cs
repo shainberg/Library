1f4d023a0334a228dd31d3d5e5d4496d65032304
adisimantov <sadis66@gmail.com>
2015-10-11 14:34:26 +0300

    book and layout

--- Library/Controllers/BooksController.cs
+++ Library/Controllers/BooksController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
@@ -13,10 +12,10 @@ namespace Library.Controllers
 {
     public class BooksController : Controller
     {
-        private paraContext db = new paraContext();
+        public Book b = new Book();
 
         // GET: Books
-        public async Task<ActionResult> Index()
+        public ActionResult Index()
         {
             //Book b = new Book();
             //b.id = 1;
@@ -31,111 +30,30 @@ namespace Library.Controllers
             //await db.SaveChangesAsync();
            
 
-            return View(await db.books.ToListAsync());
+            return View(b.getAllBooks());
         }
 
-        // GET: Books/Details/5
-        public async Task<ActionResult> Details(int? id)
+        public JsonResult getAllBooks()
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Book book = await db.books.FindAsync(id);
-            if (book == null)
-            {
-                return HttpNotFound();
-            }
-            return View(book);
+            return Json(b.getAllBooks(), JsonRequestBehavior.AllowGet);
         }
 
-        // GET: Books/Create
-        public ActionResult Create()
+        public JsonResult getDetails(int id)
         {
-            return View();
+            return Json(b.getBookByID(id), JsonRequestBehavior.AllowGet);
         }
 
-        // POST: Books/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "id,title,author,publisher,publicationYear,language,series,summery,number,category,copies,trailerURL,picture")] Book book)
+        [HttpDelete]
+        public Boolean removeMovieByID(String id)
         {
-            if (ModelState.IsValid)
+            //TODO: if current user is admin
+            //if (!User.Identity.Name.Equals("") && MovieTheater.Models.User.isAdmin(User.Identity.Name))
+            if(true)
             {
-                db.books.Add(book);
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
+                return (b.deleteBook(id));
             }
 
-            return View(book);
-        }
-
-        // GET: Books/Edit/5
-        public async Task<ActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Book book = await db.books.FindAsync(id);
-            if (book == null)
-            {
-                return HttpNotFound();
-            }
-            return View(book);
-        }
-
-        // POST: Books/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "id,title,author,publisher,publicationYear,language,series,summery,number,category,copies,trailerURL,picture")] Book book)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(book).State = EntityState.Modified;
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            return View(book);
-        }
-
-        // GET: Books/Delete/5
-        public async Task<ActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Book book = await db.books.FindAsync(id);
-            if (book == null)
-            {
-                return HttpNotFound();
-            }
-            return View(book);
-        }
-
-        // POST: Books/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> DeleteConfirmed(int id)
-        {
-            Book book = await db.books.FindAsync(id);
-            db.books.Remove(book);
-            await db.SaveChangesAsync();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
+            return false;
         }
     }
 }
---
+++ Library/Scripts/paradise/book.js
@@ -0,0 +1,95 @@
+﻿
+$(document).ready(function() {
+    $("a[href='#all-books']").on('click', function () {
+        showAllBooks();
+    });
+});
+
+function openDetailsModal(id) {
+    $.ajax({
+        dataType: "json",
+        url: "Books/getDetails",
+        data: { "id": id },
+        success: function (book) {
+            debugger;
+            $('#detailsBookTitle').text(book.title);
+            $('#detailsBookSeries').text('(' + book.series + ((book.number) ? ' #' + book.number : "") + ')');
+            $('#detailsBookAuthor').text('by ' + book.author);
+            $('#detailsCopies').text(book.copies + ' copies available')
+            $('#detailsBookSummery').text(book.summery);
+            $('#detailsBookPublishData').text('published at ' + book.publicationYear + ' by ' + book.publisher + ', ' + book.language);
+            $('#detailsBookCategory').text(book.category);
+        }});
+
+    $('#detailsModal').modal('show');
+
+
+    //$('#detailsModal').on('shown.bs.modal', function () {
+    //    book = getDetails(id);
+    //    debugger;
+    //    $('#detailsBookTitle').text(book.title);
+    //    $('#detailsBookSeries').text('(' + book.series + ((book.number) ? ' #' + book.number : "") + ')');
+    //    $('#detailsBookAuthor').text('by ' + book.author);
+    //    $('#detailsCopies').text(book.copies + ' copies available')
+    //    $('#detailsBookSummery').text(book.summery);
+    //    $('#detailsBookPublishData').text('published at ' + book.publicationYear + ' by ' + book.publisher + ', ' + book.language);
+    //    $('#detailsBookCategory').text(book.category);
+
+    //    $('#deleteBook').click(function () {
+
+    //    });
+    //});
+};
+
+
+function showAllBooks() {
+    $.ajax({
+        url: "Books/getAllBooks",
+        type: "GET",
+        success: function (data) {
+            debugger;
+            if ($.fn.DataTable.isDataTable($('#books'))) {
+                var oTable = $('#books').dataTable();
+            }
+            else {
+                var oTable = $('#books').dataTable({
+                    "autoWidth": false,
+                    "ordering": false,
+                    "columnDefs": [{
+                        "targets": [5],
+                        "mData":"id",
+                        "mRender": function (data, type, full) {
+                            debugger;
+                            return "<a onclick='openDetailsModal("+full[3]+")')>See Details</a>";
+                        }
+                    }]
+                });
+            }
+            oTable.fnClearTable();
+
+            var booksJson = [];
+
+            $.each(data, function (idx, obj) {
+                booksJson.push([obj.title, obj.author, obj.series, obj.number, obj.publicationYear]);
+            });
+
+            oTable.fnAddData(booksJson);
+        }
+    });
+}
+
+
+function getDetails(id) {
+    var book;
+    $.ajax({
+        dataType: "json",
+        url: "/Books/getDetails",
+        data: { "id": id },
+        async: false
+    })
+        .success(function (data) {
+            book = data;
+        });
+    debugger;
+    return book;
+};

84c89e37de1a33efc89ac35f48f92d60a4efcb55
adisimantov <sadis66@gmail.com>
2015-10-13 23:16:22 +0300

    still user......

---
+++ Library/Controllers/BorrowerController.cs
@@ -0,0 +1,58 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library.Controllers
+{
+    public class BorrowerController : Controller
+    {
+        Borrower b = new Borrower();
+
+        // GET: Readers
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult getCurrentBorrower()
+        {
+            object oId = this.Session["connected"];
+            if (oId != null)
+            {
+                return getBorrowerByUserId(Convert.ToInt32(oId));
+            }
+
+            return null;
+        }
+
+        public JsonResult getAllBorrowers()
+        {
+            return Json(b.getAllBorrowers(), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult getBorrowerByUserId(int userId)
+        {
+            return Json(b.getBorrowerByUserID(userId), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult updateBorrower([Bind(Include = "id,userId,firstName,lastName,sex,phone,address,mail")] Borrower borrower)
+        {
+            string message = "";
+            if (ModelState.IsValid) {
+                if (!b.updateBorrower(borrower))
+                {
+                    message = "there's a problem.... try again later";
+                }
+            }
+            else
+            {
+                message = "the borrower's parameters are not valid";
+            }
+
+            return (Json(message, JsonRequestBehavior.AllowGet));
+        }
+    }
+}
